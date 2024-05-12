c76e59598b8fa5bb884eb0234fa364d83aee6dd8
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-12 20:42:25 +0900

    Title: 특정 조건 세 정수 비교, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240512/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240512/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,9 +1,9 @@
 num_list = list(map(int, input().split()))
 
 if (num_list[0] == min(num_list)):
-    print(1)
+    print(1, end=' ')
 else:
-    print(0)
+    print(0, end=' ')
 
 if (num_list[0] == num_list[1] and num_list[1] == num_list[2]):
     print(1)
