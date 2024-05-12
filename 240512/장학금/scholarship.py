028cf2bcbcf55ea91a78d4689b4c06588a984d00
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-12 20:56:28 +0900

    Title: 장학금, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240512/장학금/scholarship.py
@@ -0,0 +1,11 @@
+mid, final = map(int, input().split())
+
+if mid >= 90:
+    if final >= 95:
+        print(100000)
+    elif final >= 90:
+        print(50000)
+    else:
+        print(0)
+else:
+    print(0)
