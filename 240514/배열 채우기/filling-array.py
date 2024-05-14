fde43f74889325fe875a09dff277c9f46b7081c7
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-14 12:32:45 +0900

    Title: 배열 채우기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240514/배열 채우기/filling-array.py
+++ 240514/배열 채우기/filling-array.py
@@ -1,4 +1,8 @@
 num_list = list(map(int, input().split()))
 
-for i in range(len(num_list) -1):
-    print(num_list[len(num_list) - i - 2], end=' ')
+if num_list[-1] == 0:
+    for i in range(len(num_list) -1):
+        print(num_list[len(num_list) - i - 2], end=' ')
+else:
+    for i in range(len(num_list)):
+        print(num_list[len(num_list) - i - 1], end=' ')
