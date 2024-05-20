652526ec186e40505a5165249b77960743e3c2b5
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-20 19:15:48 +0900

    Title: 방향에 맞춰 이동, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240520/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+x,y = 0, 0
+for _ in range(n):
+    dir, num = input().split()
+    if dir == 'N':
+        y += int(num)
+    if dir == 'S':
+        y -= int(num)
+    if dir == 'E':
+        x += int(num)
+    if dir == 'W':
+        x += int(num)
+
+print(x, y)
