405a86af4f71a8a917b4acb9d696ccc27b7bf36c
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-20 22:07:25 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240520/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240520/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -1,28 +1,33 @@
-height, width = map(int, input().split())
+y_max, x_max = map(int, input().split())
 
 def in_range(x, y):
-    return 0<=x and x < width and 0 <= y and y < height
+    return 0<=x and x < x_max and 0 <= y and y < y_max
 
-written_check = [[0 for _ in range(width)] for _ in range(height)]
-result = [[0 for _ in range(width)] for _ in range(height)]
+written_check = [[0 for _ in range(x_max)] for _ in range(y_max)]
+result = [[0 for _ in range(x_max)] for _ in range(y_max)]
 
-dxs = [1,0,-1,0]
-dys = [0,-1,0,1]
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
 
 x=0
 y=0
 direction = 0
 
-for i in range(height * width):
-    if not in_range or written_check[x][y] == 1:
+for i in range(y_max * x_max):
+
+    written_check[y][x] = 1
+    result[y][x] = i+1
+
+    next_x = x + dxs[direction]
+    next_y = y + dys[direction]
+
+    if not in_range(next_x, next_y) or written_check[next_y][next_x] == 1:
         direction = (direction + 1) % 4
+        next_x = x + dxs[direction]
+        next_y = y + dys[direction]
 
-    written_check[x][y] = 1
-    result[x][y] = i+1
+    x = next_x
+    y = next_y
 
-    x += dxs[direction]
-    y += dys[direction]
-      
-
-for _ in range(height):
-    print(result)
+for i in range(y_max):
+    print(*result[i])
