820a478f15ca445300204ba02580d6be8603c35b
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 20:38:24 +0900

    Title: 체크판위에서 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/체크판위에서 2/on-the-checkboard-2.py
+++ 240522/체크판위에서 2/on-the-checkboard-2.py
@@ -4,15 +4,18 @@ lst = [list(input().split()) for _ in range(height)]
 
 first = lst[0][0]
 other = lst[height-1][width-1]
+if first != other:
+    cnt = 0
 
-cnt = 0
-for i in range(1,height-1):
-    for j in range(1, width-1):
-        if lst[i][j] == other:
-            #print(i, j, end='\n----------\n')
-            for k in range(i+1, height-1):
-                for l in range(j+1, width-1):
-                    if lst[k][l] == first:
-                        #print(k, l, cnt+1)
-                        cnt += 1
-print(cnt)
+    for i in range(1,height-1):
+        for j in range(1, width-1):
+            if lst[i][j] == other:
+                #print(i, j, end='\n----------\n')
+                for k in range(i+1, height-1):
+                    for l in range(j+1, width-1):
+                        if lst[k][l] == first:
+                            #print(k, l, cnt+1)
+                            cnt += 1
+    print(cnt)
+else:
+    print(0)
