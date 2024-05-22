d790cb6374bd171637376552c34d5b7905a07ff8
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 20:57:04 +0900

    Title: 이상한 진수 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/이상한 진수 2/awkward-digits-2.py
+++ 240522/이상한 진수 2/awkward-digits-2.py
@@ -2,12 +2,18 @@ lst = list(input())
 for i in range(len(lst)):
     lst[i] = int(lst[i])
 #print(lst)
+count_1 = 0
 for i in range(len(lst)):
     if lst[i] == 0:
         lst[i] = 1
         break
     else:
-        continue
+        count_1 += 1
+        if count_1 != len(lst):
+            continue            
+        else:
+            lst[-1] = 0
+            break
 #print(lst)
 
 res = 0
