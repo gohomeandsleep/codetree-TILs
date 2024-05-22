80be39bd6fc7232a14ccee3067314753b52cf212
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 22:31:24 +0900

    Title: 오목, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240522/오목/O-mok.py
@@ -0,0 +1,16 @@
+lst = [list(map(int, input().split())) for _ in range(19)]
+
+for i in range(19):
+    for j in range(19):
+        if lst[i][j] != 0:
+            primary = lst[i][j]
+            #case1. 직선으로 같을때
+            if lst[i][j+1] == primary and lst[i][j+2] == primary and lst[i][j+3] == primary and lst[i][j+4] == primary:
+                print(primary)
+                print(i+1, j+3)
+            if lst[i+1][j] == primary and lst[i+2][j] == primary and lst[i+3][j] == primary and lst[i+4][j] == primary:
+                print(primary)
+                print(i+3, j+1)
+            if lst[i+1][j+1] == primary and lst[i+2][j+2] == primary and lst[i+3][j+3] == primary and lst[i+4][j+4] == primary:
+                print(primary)
+                print(i+3, j+3)
