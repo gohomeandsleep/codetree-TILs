f3d6e8fe8abcac63ab5fbbcfba1e856967637da1
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 22:37:02 +0900

    Title: 오목, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/오목/O-mok.py
+++ 240522/오목/O-mok.py
@@ -1,27 +1,39 @@
 lst = [list(map(int, input().split())) for _ in range(19)]
 
 ans = 0
-for i in range(15):
-    for j in range(15):
+for i in range(19):
+    for j in range(19):
         if lst[i][j] != 0:
             primary = lst[i][j]
             #case1. 직선으로 같을때
-            if lst[i][j+1] == primary and lst[i][j+2] == primary and lst[i][j+3] == primary and lst[i][j+4] == primary:
-                print(primary)
-                print(i+1, j+3)
-                ans = 1
-            if lst[i+1][j] == primary and lst[i+2][j] == primary and lst[i+3][j] == primary and lst[i+4][j] == primary:
-                print(primary)
-                print(i+3, j+1)
-                ans = 1
-            if lst[i+1][j+1] == primary and lst[i+2][j+2] == primary and lst[i+3][j+3] == primary and lst[i+4][j+4] == primary:
-                print(primary)
-                print(i+3, j+3)
-                ans = 1
-            if lst[i+1][j-1] == primary and lst[i+2][j-2] == primary and lst[i+3][j-3] == primary and lst[i+4][j-4] == primary:
-                print(primary)
-                print(i+3, j-1)
-                ans = 1
+            try: 
+                if lst[i][j+1] == primary and lst[i][j+2] == primary and lst[i][j+3] == primary and lst[i][j+4] == primary:
+                    print(primary)
+                    print(i+1, j+3)
+                    ans = 1
+            except:
+                continue
+            try: 
+                if lst[i+1][j] == primary and lst[i+2][j] == primary and lst[i+3][j] == primary and lst[i+4][j] == primary:
+                    print(primary)
+                    print(i+3, j+1)
+                    ans = 1
+            except:
+                continue
+            try: 
+                if lst[i+1][j+1] == primary and lst[i+2][j+2] == primary and lst[i+3][j+3] == primary and lst[i+4][j+4] == primary:
+                    print(primary)
+                    print(i+3, j+3)
+                    ans = 1
+            except:
+                continue
+            try:
+                if lst[i+1][j-1] == primary and lst[i+2][j-2] == primary and lst[i+3][j-3] == primary and lst[i+4][j-4] == primary:
+                    print(primary)
+                    print(i+3, j-1)
+                    ans = 1
+            except:
+                continue
 
 if ans == 0:
     print(0)
