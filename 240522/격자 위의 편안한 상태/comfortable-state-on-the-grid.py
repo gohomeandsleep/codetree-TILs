d21c60b545d0faf2a572dbfcca54612f325fbe3d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 18:51:10 +0900

    Title: 격자 위의 편안한 상태, Time: 188ms, Memory: 26MB, Status: Passed - CodeTree

--- 240522/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240522/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -5,20 +5,18 @@ lst = [[0 for _ in range(n)] for _ in range(n)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
 for _ in range(m):
     r, c = map(int, input().split())
     lst[r-1][c-1] = 1
 
-
     cnt = 0
     for i in range(4):
-        try:
-            if r-1-dx[i] < 0 or c-1+dy[i] < 0:
-                continue
-            elif lst[r-1+dx[i]][c-1+dy[i]] == 1:
-                cnt += 1
-        except:
-            cnt += 0
+        nx, ny = r-1+dx[i], c-1+dy[i]
+        if in_range(nx, ny) and lst[nx][ny] == 1:
+            cnt += 1
     if cnt == 3:
         print(1)
     else:
