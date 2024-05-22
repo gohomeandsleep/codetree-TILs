079fcf40b9dec55f9474993dbbc925a9bcf97fcf
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 22:33:46 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240522/오목/O-mok.py
+++ 240522/오목/O-mok.py
@@ -1,5 +1,6 @@
 lst = [list(map(int, input().split())) for _ in range(19)]
 
+ans = 0
 for i in range(19):
     for j in range(19):
         if lst[i][j] != 0:
@@ -8,12 +9,19 @@ for i in range(19):
             if lst[i][j+1] == primary and lst[i][j+2] == primary and lst[i][j+3] == primary and lst[i][j+4] == primary:
                 print(primary)
                 print(i+1, j+3)
+                ans = 1
             if lst[i+1][j] == primary and lst[i+2][j] == primary and lst[i+3][j] == primary and lst[i+4][j] == primary:
                 print(primary)
                 print(i+3, j+1)
+                ans = 1
             if lst[i+1][j+1] == primary and lst[i+2][j+2] == primary and lst[i+3][j+3] == primary and lst[i+4][j+4] == primary:
                 print(primary)
                 print(i+3, j+3)
+                ans = 1
             if lst[i+1][j-1] == primary and lst[i+2][j-2] == primary and lst[i+3][j-3] == primary and lst[i+4][j-4] == primary:
                 print(primary)
                 print(i+3, j-1)
+                ans = 1
+
+if ans == 0:
+    print(0)
