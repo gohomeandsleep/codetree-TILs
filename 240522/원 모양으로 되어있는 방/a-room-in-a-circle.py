8f908a8799cd6ff8d2db69f3506ac545e9837ca1
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 21:48:30 +0900

    Title: 원 모양으로 되어있는 방, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240522/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(int(input()))
+
+min_sum = sys.maxsize
+for i in range(n):
+    #i : 시작할 방
+    sum = 0
+    for j in range(n-1):
+        try:
+            sum += (j+1) * lst[i+j+1]
+            #print((j+1) * lst[i+j+1], end=' ')
+        except:
+            sum += (j+1) * lst[i+j+1-5]
+            #print((j+1) * lst[i+j+1-5], end=' ')
+    #print(sum)
+
+    if sum < min_sum:
+        min_sum = sum
+        #print(sum)
+
+print(min_sum)
