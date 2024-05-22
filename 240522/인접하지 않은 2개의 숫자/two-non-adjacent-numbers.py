6690b34856649fe61589a5f287e6c91c3605a8df
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 22:02:33 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240522/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240522/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -3,10 +3,11 @@ import sys
 n = int(input())
 lst = list(map(int, input().split()))
 max = -sys.maxsize
-for i in range(1, n):
+for i in range(n):
     #i : 첫번째 인자
     for j in range(i+2, n):
         #j : 두번째 인자
+        #print(lst[i] + lst[j])
         if lst[i] + lst[j] > max:
             max = lst[i] + lst[j]
 
