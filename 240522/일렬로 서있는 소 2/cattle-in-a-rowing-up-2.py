ee3212d242a47e69dbeb4bb6cee3a19fddf60a5f
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-05-22 19:41:56 +0900

    Title: 일렬로 서있는 소 2, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240522/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,14 @@
+n = int(input())
+lst = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        if lst[i] <= lst[j]:
+            for k in range(j+1, n):
+                if lst[j] <= lst[k]:
+                    cnt +=1
+
+
+
+print(cnt)
