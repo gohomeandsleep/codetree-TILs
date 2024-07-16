e894734c789e1b741e9dacccac148f92f4242595
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 18:09:55 +0900

    Title: 구구단 만들기 2, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240716/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+for i in range(2, 10, 2):
+    for j in range(m, n-1, -1):
+        print("%d * %d = %d" % (j, i, i*j), end='')
+        if j != n:
+            print(' /', end=' ')
+    print('')
