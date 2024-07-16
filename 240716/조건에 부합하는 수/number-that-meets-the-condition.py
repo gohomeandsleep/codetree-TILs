780183e38d10edb62db4652239dfefa387ab2828
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 11:07:05 +0900

    Title: 조건에 부합하는 수, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240716/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 2 == 0 and i % 4 != 0:
+        continue
+    elif (i // 8) % 2 == 0:
+        continue
+    elif (i % 7) < 4:
+        continue
+    else:
+        print(i, end=' ')
