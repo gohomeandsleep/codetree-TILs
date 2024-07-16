020f3ad8b2454bdc81e14943d6928cbf6f9ab14d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 10:46:18 +0900

    Title: 완전수 판별기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/완전수 판별기/perfect-number-discriminator.py
+++ 240716/완전수 판별기/perfect-number-discriminator.py
@@ -1,20 +1,36 @@
 prim = int(input())
 n = prim
-lst = set()
-lst.add(1)
+lst = []
+freq = []
 while n % 2 == 0:
-    lst.add(2)
+    if 2 not in lst:
+        lst.append(2)
+        freq.append(1)
+    else:
+        freq[0] += 1
     n //= 2
 
-for i in range(3, int(n ** 0.5) + 1, 2):
+for i in range(3, int(n ** 0.5), 2):
     if n % i == 0:
-        lst.add(i)
+        if i not in lst:
+            lst.append(i)
+            freq.append(1)
+        else:
+            freq[lst.index(i)] += 1
         n //= i
 
 if n > 2:
-    lst.add(n)
+    lst.append(n)
+    freq.append(1)
 
-if sum(lst) == prim:
+res = 1
+for i in range(len(lst)):
+    tmp = 1
+    for j in range(1, freq[i]+1):
+        tmp += lst[i] ** j
+    res *= tmp
+
+if res // 2 == prim:
     print('P')
 else:
     print('N')
