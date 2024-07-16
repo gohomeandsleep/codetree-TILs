583f96c24da50fba43dbdf5a605726b21e1459a8
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 10:54:07 +0900

    Title: 친근하지 않은 수, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/친근하지 않은 수/unfriendly-number.py
+++ 240716/친근하지 않은 수/unfriendly-number.py
@@ -2,7 +2,7 @@ n = int(input())
 
 res = 0
 for i in range(1, n+1):
-    if n % 2 != 0 and n % 3 != 0 and n % 5 != 0:
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
         res += 1
 
 print(res)
