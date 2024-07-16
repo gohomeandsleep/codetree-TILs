6f9db20fe1bd7d005d10b7229954b2516fac8d8b
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 11:38:01 +0900

    Title: 공약수의 유무, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240716/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -7,7 +7,7 @@ a, b = map(int, input().split())
 
 res = False
 for i in range(a, b+1):
-    if gcd(i, 1920) != 1 and gcd(i, 2880) != 1:
+    if gcd(i, 960) != 1:
         res = True
 
 if res == True:
