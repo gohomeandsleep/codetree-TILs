83484df1ee23b28692dfdb9e8fc1c24d406895b6
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 09:48:31 +0900

    Title: a 부터 b 까지, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+print(a, end=' ')
+while a < b:
+    if a % 2 == 0:
+        a += 3
+        print(a, end=' ')
+    else:
+        a *= 2
+        print(a, end=' ')
