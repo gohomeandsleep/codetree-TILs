8770063253ccb526c863b0302e49f7ad80dd93e8
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-16 18:42:12 +0900

    Title: 3n + 1 수열 m번, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240716/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for i in range(T):
+    n = int(input())
+    cnt = 0
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+        cnt +=1
+    print(cnt)
