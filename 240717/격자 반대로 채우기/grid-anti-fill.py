5f25ded4b0ee751e5105787f00ee0d471e7f7b33
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-17 11:48:42 +0900

    Title: 격자 반대로 채우기, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/격자 반대로 채우기/grid-anti-fill.py
+++ 240717/격자 반대로 채우기/grid-anti-fill.py
@@ -2,15 +2,26 @@ n = int(input())
 lst = [[0] * n for _ in range(n)]
 
 val = 1
-for i in range(n-1, -1, -1):
-    if i % 2 != 0:
-        for j in range(n-1, -1, -1):
-            lst[j][i] = val
-            val += 1
-    else:
-        for j in range(n):
-            lst[j][i] = val
-            val += 1
+if n % 2 == 0:
+    for i in range(n-1, -1, -1):
+        if i % 2 != 0:
+            for j in range(n-1, -1, -1):
+                lst[j][i] = val
+                val += 1
+        else:
+            for j in range(n):
+                lst[j][i] = val
+                val += 1
+else:
+    for i in range(n-1, -1, -1):
+        if i % 2 == 0:
+            for j in range(n-1, -1, -1):
+                lst[j][i] = val
+                val += 1
+        else:
+            for j in range(n):
+                lst[j][i] = val
+                val += 1
 
 for row in lst:
     print(' '.join(map(str, row)))
