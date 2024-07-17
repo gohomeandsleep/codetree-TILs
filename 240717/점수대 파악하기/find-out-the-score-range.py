251efa8a42c7cd4210899513e100f974767fdf79
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-17 09:59:40 +0900

    Title: 점수대 파악하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,12 @@
+lst = list(map(int, input().split()))
+res = [0 for _ in range(10)]
+
+for i in range(len(lst)):
+    if lst[i] == 0:
+        break
+    tmp = lst[i] // 10
+    res[tmp-1] += 1
+
+res = res[::-1]
+for i in range(10):
+    print("%d - %d" % (10 * (10-i), res[i]))
