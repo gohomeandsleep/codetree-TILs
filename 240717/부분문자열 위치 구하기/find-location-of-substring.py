3b44190c1eaeab3ef68cb7152461db3814569871
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-17 14:33:37 +0900

    Title: 부분문자열 위치 구하기, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,11 @@
+lst = list(input())
+c = list(input())
+
+stat = True
+for i in range(len(lst) - len(c) + 1):
+    if lst[i:i+len(c)] == c:
+        stat = False
+        print(i)
+
+if stat == True:
+    print(-1)
