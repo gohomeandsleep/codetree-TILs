0d5d7bb26fa275a204443a71d327d9471939953a
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-17 12:55:06 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 171ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -0,0 +1,14 @@
+lst = ['apple', 'banana', 'grape', 'blueberry', 'orange']
+llist = []
+
+for i in range(len(lst)):
+    llist.append(list(lst[i]))
+
+c = input()
+res = []
+for i in range(5):
+    if llist[i][2] == c or llist[i][3] == c:
+        res.append(lst[i])
+
+print(*res, sep='\n')
+print(len(res))
