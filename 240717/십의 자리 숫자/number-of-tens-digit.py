36a3d749477e5e87613f0b284eaf444cddad57d9
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-17 09:05:30 +0900

    Title: 십의 자리 숫자, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/십의 자리 숫자/number-of-tens-digit.py
+++ 240717/십의 자리 숫자/number-of-tens-digit.py
@@ -2,6 +2,8 @@ lst = list(map(int, input().split()))
 res = [0 for _ in range(9)]
 
 for i in range(len(lst)):
+    if lst[i] == 0:
+        break
     tmp = lst[i] // 10
     if tmp >= 1:
         res[tmp-1] += 1
