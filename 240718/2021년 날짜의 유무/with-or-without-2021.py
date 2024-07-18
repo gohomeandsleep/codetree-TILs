f3e2033a950e0abee1722a9a4da1dd0c40f54b20
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-18 10:34:22 +0900

    Title: 2021년 날짜의 유무, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/2021년 날짜의 유무/with-or-without-2021.py
+++ 240718/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,7 +2,10 @@ m, d = map(int, input().split())
 
 lst = [31, 28, 31,30,31,30,31,31,30,31,30,31]
 
-if d > lst[m-1]:
-    print("No")
-else:
-    print('Yes')
+try:
+    if d > lst[m-1]:
+        print("No")
+    else:
+        print('Yes')
+except:
+    print('No')
