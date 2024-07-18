7f493a928a62d6e4cf812d4f89c486c1d3f4722b
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-18 13:58:58 +0900

    Title: 비오는 날, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/비오는 날/rainy-day.py
+++ 240718/비오는 날/rainy-day.py
@@ -1,6 +1,19 @@
+class date:
+    def __init__(self, d, t):
+        self.d = d
+        self.t = t
+
+    def __str__(self):
+        return f"{self.d} {self.t}"
+
 n = int(input())
+lst = []
 for _ in range(n):
     d, t, w = input().split()
     if w == 'Rain':
-        print(d, t, w)
-        break
+        lst.append(date(d, t))
+
+if lst:
+    lst.sort(key=lambda x: x.d)
+    print(lst[0], end=' ')
+    print('Rain')
