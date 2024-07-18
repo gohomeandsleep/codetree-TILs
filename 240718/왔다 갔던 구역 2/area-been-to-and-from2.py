596c8858bfa3aff26fad8b1690912ee65282b50d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-18 16:16:45 +0900

    Title: 왔다 갔던 구역 2, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240718/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240718/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,15 +7,17 @@ for _ in range(n):
     if d == 'R':
         for i in range(cur, cur + int(l)):
             lst[i] += 1
-        cur += int(l)
+            cur += 1
     else:
-        for i in range(cur, cur - int(l)-1, -1):
+        for i in range(cur-1, cur - int(l) -1, -1):
             lst[i] += 1
-        cur -= int(l)
+            cur -= 1
     #print(cur)
 
 res = 0
 for i in range(2000):
+    #if lst[i] != 0:
+        #print(i, lst[i])
     if lst[i] >= 2:
         res += 1
 print(res)
