9515d58a4e7d3a0df499e55499c3fd55b7b62810
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-18 15:58:25 +0900

    Title: 왔다 갔던 구역 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240718/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,21 @@
+n = int(input())
+lst = [0 for _ in range(2000)]
+
+cur = 1000
+for _ in range(n):
+    l, d = input().split()
+    if d == 'R':
+        for i in range(cur, cur + int(l)):
+            lst[i] += 1
+        cur += int(l)
+    else:
+        for i in range(cur, cur - int(l), -1):
+            lst[i] += 1
+        cur -= int(l)
+    #print(cur)
+
+res = 0
+for i in range(2000):
+    if lst[i] >= 2:
+        res += 1
+print(res)
