769255b855a4f4d6a3ab7a465d90876ddb03f8e6
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-18 17:15:28 +0900

    Title: 흰검 칠하기, Time: 159ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240718/흰검 칠하기/painting-white-black.py
+++ 240718/흰검 칠하기/painting-white-black.py
@@ -9,19 +9,20 @@ for _ in range(n):
         for i in range(cur, cur + int(l)):
             lst[i] = 'B'
             num[i] += 1
-            cur += 1
+        cur += int(l)-1
     else:
-        for i in range(cur-1, cur - int(l) -1, -1):
+        for i in range(cur, cur - int(l), -1):
             lst[i] = 'W'
             num[i] += 1
-            cur -= 1
+        cur -= int(l)-1
     #print(cur)
 
 black = 0
 grey = 0
 white = 0
-res = 0
 for i in range(200000):
+    #if num[i] != 0:
+        #print(i, num[i])
     if num[i] >= 4:
         grey += 1
     elif lst[i] == 'B':
