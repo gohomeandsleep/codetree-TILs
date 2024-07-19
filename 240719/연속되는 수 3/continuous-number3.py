1841e3cd541b932a5ddaafb43a3f7ecb72f21f2f
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-19 10:17:38 +0900

    Title: 연속되는 수 3, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240719/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,20 @@
+n = int(input())
+lst = [int(input()) for _ in range(n)]
+
+res = 0
+tmp = 1
+for i in range(1, n):
+    if (lst[i-1] >0 and lst[i] > 0) or (lst[i-1] < 0 and lst[i] < 0):
+        tmp += 1
+    else:
+        if res <= tmp:
+            res = tmp
+        tmp = 1
+    #print(tmp, res)
+if res <= tmp:
+    res = tmp
+
+if len(lst) == 1:
+    print(1)
+else:
+    print(res)
