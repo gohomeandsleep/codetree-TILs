06289c7cca2b2936dbe68cb8e81187080333f421
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-19 10:31:26 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240719/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -6,19 +6,22 @@ if lst[0] > k:
     tmp = 1
 else:
     tmp = 0
-    
+
 for i in range(1, n):
-    if lst[i] > k:
+    if lst[i] > k and lst[i] > lst[i-1]:
         tmp += 1
     else:
         if res <= tmp:
             res = tmp
         tmp = 1
-    #print(tmp, res)
+
 if res <= tmp:
     res = tmp
 
 if len(lst) == 1:
-    print(1)
+    if lst[0] > k:
+        print(1)
+    else:
+        print(0)
 else:
     print(res)
