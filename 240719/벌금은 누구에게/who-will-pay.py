14544e59d87208676e9ef7ef271a8961718b5a16
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-19 14:07:21 +0900

    Title: 벌금은 누구에게, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240719/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,10 @@
+std, T, k = map(int, input().split())
+lst = [0 for _ in range(std)]
+res = -1
+for i in range(T):
+    n = int(input())
+    lst[n-1] += 1
+    if lst[n-1] == 3:
+        res = n
+
+print(res)
