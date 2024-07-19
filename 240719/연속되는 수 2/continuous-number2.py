994715938b7a9dae88b1e2780c2503db77601d84
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-19 10:14:53 +0900

    Title: 연속되는 수 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = [int(input()) for _ in range(n)]
+
+res = 0
+tmp = 1
+for i in range(1, n):
+    if lst[i-1] == lst[i]:
+        tmp += 1
+    else:
+        if res <= tmp:
+            res = tmp
+        tmp = 1
+    #print(tmp, res)
+
+print(res)
