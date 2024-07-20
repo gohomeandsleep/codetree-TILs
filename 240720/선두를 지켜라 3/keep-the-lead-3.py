0f528ba8f07ab59154e03a30e6f54cd97241cdda
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-20 11:39:21 +0900

    Title: 선두를 지켜라 3, Time: 202ms, Memory: 50MB, Status: Passed - CodeTree

---
+++ 240720/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,42 @@
+n, m = map(int, input().split())
+
+alst = []
+blst = []
+a = 0
+b = 0 
+for _ in range(n):
+    va, ta = map(int, input().split())
+    for i in range(ta):
+        a += va
+        alst.append(a)
+
+for _ in range(m):
+    vb, tb = map(int, input().split())
+    for i in range(tb):
+        b += vb
+        blst.append(b)
+
+res = []
+cnt = 1
+if alst[0] > blst[0]:
+    res.append(0)
+elif alst[0] < blst[0]:
+    res.append(1)
+else:
+    res.append(2)
+
+for i in range(len(alst)):
+    if alst[i] > blst[i]:
+        if res[-1] != 0:
+            cnt += 1
+        res.append(0)
+    elif alst[i] < blst[i]:
+        if res[-1] != 1:
+            cnt += 1
+        res.append(1)
+    else:
+        if res[-1] != 2:
+            cnt += 1
+        res.append(2)
+
+print(cnt)
