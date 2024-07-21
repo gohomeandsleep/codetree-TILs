8346c40e0b9e19b909390b82f9c1eaa5a5ed1be7
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-21 20:50:54 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240721/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -7,13 +7,16 @@ dy = [0, -1, 0, 1]
 x, y, d = n-1, n-1, 0
 cnt = n * n
 
-while cnt > 0:
-    lst[y][x] = cnt
-    cnt -= 1
-    x += dx[d]
-    y += dy[d]
-    if x+dx[d] == n or y+dy[d] == n or lst[y+dy[d]][x+dx[d]] != 0:
-        d = (d + 1) % 4
+if n == 1:
+    lst[0][0] = 1
+else:
+    while cnt > 0:
+        lst[y][x] = cnt
+        cnt -= 1
+        x += dx[d]
+        y += dy[d]
+        if x+dx[d] == n or y+dy[d] == n or lst[y+dy[d]][x+dx[d]] != 0:
+            d = (d + 1) % 4
 
 for row in lst:
     print(*row, sep=' ')
