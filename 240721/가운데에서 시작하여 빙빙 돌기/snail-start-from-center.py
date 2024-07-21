47edb0088363d3b18f4a456f63e0ad792a983428
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-21 20:50:19 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240721/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,19 @@
+n = int(input())
+lst = [[0] * n for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+x, y, d = n-1, n-1, 0
+cnt = n * n
+
+while cnt > 0:
+    lst[y][x] = cnt
+    cnt -= 1
+    x += dx[d]
+    y += dy[d]
+    if x+dx[d] == n or y+dy[d] == n or lst[y+dy[d]][x+dx[d]] != 0:
+        d = (d + 1) % 4
+
+for row in lst:
+    print(*row, sep=' ')
