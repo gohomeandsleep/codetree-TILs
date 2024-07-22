4339aa88d608418760d0ad3c29711571c7634f4b
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-22 16:28:36 +0900

    Title: 최고의 13위치 2, Time: 204ms, Memory: 26MB, Status: Passed - CodeTree

--- 240722/최고의 13위치 2/best-place-of-13-2.py
+++ 240722/최고의 13위치 2/best-place-of-13-2.py
@@ -1,26 +1,17 @@
 n = int(input())
 lst = [list(map(int, input().split())) for _ in range(n)]
-first = 0
-second = 0
-third = 0
-for y in range(n):
-    prev_x = 0
-    for x in range(n-2):
-        tmp = lst[y][x] + lst[y][x+1] + lst[y][x+2]
-        if tmp >= first:
-            third = second
-            second = first
-            first = tmp
-            #print(x, prev_x, end="|\n")
-            if x < prev_x:
-                second = third
-            prev_x = x + 2
-            #print(prev_x)
-        elif tmp >= second:
-            third = second
-            second = tmp
-            if x < prev_x:
-                second = third
-           
-        #print(first, second) 
-print(first + second)
+
+cnt = 0
+for i in range(n):
+    for j in range(n - 2):
+        for k in range(n):
+            for l in range(n - 2):
+                if i == k and abs(j - l) <= 2: 
+                    continue
+                else:
+                    cnt1 = lst[i][j] + lst[i][j+1] + lst[i][j+2]
+                    cnt2 = lst[k][l] + lst[k][l+1] + lst[k][l+2]
+                    cnt = max(cnt, cnt1 + cnt2)
+
+
+print(cnt)
