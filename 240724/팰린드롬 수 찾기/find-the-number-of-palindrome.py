09f9c0035018d633c2dfe4a6f4e150c626b0eb0f
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 17:13:22 +0900

    Title: 팰린드롬 수 찾기, Time: 216ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240724/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -5,7 +5,7 @@ for i in range(stp, endp + 1):
     tmp = list(str(i))
     stat = True
     for i in range(len(tmp) // 2):
-        if tmp[i] != tmp[len(tmp) - 1]:
+        if tmp[i] != tmp[len(tmp) - i - 1]:
             stat = False
     if stat == True:
         res += 1
