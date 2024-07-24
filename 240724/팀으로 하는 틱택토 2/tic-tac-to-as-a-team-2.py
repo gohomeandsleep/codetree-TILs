b7f379402723b1227d8297f1b9a00e7665d334ea
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 18:14:00 +0900

    Title: 팀으로 하는 틱택토 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240724/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -5,6 +5,7 @@ for _ in range(3):
         p[i] = int(p[i])
     lst.append(p)
 cnt = 0
+res = []
 
 for i in range(3):
     tmp = [lst[i][0], lst[i][1], lst[i][2]]
@@ -13,7 +14,8 @@ for i in range(3):
     elif tmp[0] != tmp[1] and tmp[1] != tmp[2] and tmp[0] != tmp[2]:
         cnt += 0
     else:
-        cnt += 1
+        if tmp not in res:
+            res.append(tmp)
 
 for i in range(3):
     tmp = [lst[0][i], lst[1][i], lst[2][i]]
@@ -22,7 +24,8 @@ for i in range(3):
     elif tmp[0] != tmp[1] and tmp[1] != tmp[2] and tmp[0] != tmp[2]:
         cnt += 0
     else:
-        cnt += 1
+        if tmp not in res:
+            res.append(tmp)
 
 tmp = [lst[0][0] ,lst[1][1], lst[2][2]]
 if tmp[0] == tmp[1] and tmp[1] == tmp[2]:
@@ -30,7 +33,8 @@ if tmp[0] == tmp[1] and tmp[1] == tmp[2]:
 elif tmp[0] != tmp[1] and tmp[1] != tmp[2] and tmp[0] != tmp[2]:
     cnt += 0
 else:
-    cnt += 1
+    if tmp not in res:
+        res.append(tmp)
 
 tmp = [lst[2][0] ,lst[1][1], lst[0][2]]
 if tmp[0] == tmp[1] and tmp[1] == tmp[2]:
@@ -38,6 +42,7 @@ if tmp[0] == tmp[1] and tmp[1] == tmp[2]:
 elif tmp[0] != tmp[1] and tmp[1] != tmp[2] and tmp[0] != tmp[2]:
     cnt += 0
 else:
-    cnt += 1
+    if tmp not in res:
+        res.append(tmp)
 
-print(cnt)
+print(len(res))
