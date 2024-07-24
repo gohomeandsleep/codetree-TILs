7760b5870b5e2112f75700b1493d2cd65cdf5af2
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 13:46:25 +0900

    Title: 이상한 폭탄 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+lst = []
+bomb = []
+for i in range(n):
+    tmp = int(input())
+    if tmp in lst:
+        bomb.append(tmp)
+    lst.append(tmp)
+print(max(bomb))
