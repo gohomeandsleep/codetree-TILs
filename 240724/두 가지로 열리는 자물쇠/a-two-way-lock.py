ef2de8d3af2322e62245410e10a3750b56a48939
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 10:14:07 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst1 = list(map(int, input().split()))
+lst2 = list(map(int, input().split()))
+
+res = 1
+for i in range(3):
+    dist = min(abs(lst1[i] - lst2[i]), abs(min(lst1[i], lst2[i]) - max(lst1[i], lst2[i])))
+    if dist < 5:
+        res *= (5 - dist)
+    else:
+        res = 0
+
+print(250 - res)
