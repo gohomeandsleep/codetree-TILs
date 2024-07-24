97c5d0040e55ec24a023b07430186a37eb9c302f
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 17:10:05 +0900

    Title: 빙산의 일각 2, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240724/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -12,7 +12,7 @@ for i in range(h): #해수면의 높이
     for j in range(1, n):
         if lst[j] - i <= 0 and lst[j-1] - i > 0:
             cnt += 1
-    if lst[-1] > 0:
+    if lst[-1] - i > 0:
         cnt += 1
     res = max(res, cnt)
 print(res)
