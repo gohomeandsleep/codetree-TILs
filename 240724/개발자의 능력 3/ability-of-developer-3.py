48c96cf3df91955843bff688d705865b573ecdd8
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 09:48:29 +0900

    Title: 개발자의 능력 3, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240724/개발자의 능력 3/ability-of-developer-3.py
+++ 240724/개발자의 능력 3/ability-of-developer-3.py
@@ -4,9 +4,13 @@ lst.sort(reverse=True)
 a = [0]
 b = [0]
 for i in range(6):
-    if sum(a) >= sum(b):
+    if sum(a) >= sum(b) and len(b) <= 3:
         b.append(lst[i])
-    else:
+    elif sum(a) < sum(b) and len(a) <= 3:
+        a.append(lst[i])
+    elif len(a) > 3:
+        b.append(lst[i])
+    elif len(b) > 3:
         a.append(lst[i])
 
 print(abs(sum(a) - sum(b)))
