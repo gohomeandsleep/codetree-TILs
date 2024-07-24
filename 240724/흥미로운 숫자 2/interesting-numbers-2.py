88f0f32dbad338d237deaf28d430f939fc2957f6
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 15:19:18 +0900

    Title: 흥미로운 숫자 2, Time: 224ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240724/흥미로운 숫자 2/interesting-numbers-2.py
@@ -7,8 +7,10 @@ for i in range(stp, endp + 1):
     if tmp.count(tmp[0]) == 1:
         if tmp.count(tmp[-1]) == len(tmp) - 1:
             res += 1
-    else:
+            #print(i)
+    elif tmp.count(tmp[0]) == len(tmp) - 1:
         if tmp.count(tmp[-1]) == 1:
             res += 1
-
+            #print(i)
+    
 print(res)
