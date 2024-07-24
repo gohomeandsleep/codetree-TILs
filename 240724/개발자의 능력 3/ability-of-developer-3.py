f8bfc851f2a4367fc82dd664b8fba9a6bec970bd
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 09:54:18 +0900

    Title: 개발자의 능력 3, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/개발자의 능력 3/ability-of-developer-3.py
+++ 240724/개발자의 능력 3/ability-of-developer-3.py
@@ -1,16 +1,10 @@
 lst = list(map(int, input().split()))
-lst.sort(reverse=True)
 
-a = [0]
-b = [0]
-for i in range(6):
-    if sum(a) >= sum(b) and len(b) <= 3:
-        b.append(lst[i])
-    elif sum(a) < sum(b) and len(a) <= 3:
-        a.append(lst[i])
-    elif len(a) > 3:
-        b.append(lst[i])
-    elif len(b) > 3:
-        a.append(lst[i])
-
-print(abs(sum(a) - sum(b)))
+res = float('inf')
+for i in range(len(lst)):
+    for j in range(i+1, len(lst)):
+        for k in range(j+1, len(lst)):
+            apart = lst[i] + lst[j] + lst[k]
+            bpart = sum(lst) - lst[i] - lst[j] - lst[k]
+            res = min(res, abs(apart - bpart))
+print(res)
