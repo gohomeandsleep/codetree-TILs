1480c51e19aae2fb1cbbfb7d636eaf27b679c113
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 11:00:39 +0900

    Title: 개발팀의 능력, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,16 @@
+lst = list(map(int, input().split()))
+team3 = max(lst)
+lst.remove(team3)
+
+res = float('inf')
+for i in range(4):
+    for j in range(4):
+        if i == j:
+            continue
+        team1 = lst[i] + lst[j]
+        team2 = sum(lst) - lst[i] - lst[j]
+        if team1 == team2 or team2 == team3 or team1 == team3:
+            continue
+        tmp = max(team1, team2, team3) - min(team1, team2, team3)
+        res = min(res, tmp)
+print(res)
