e68a6414a4ed97242791b577a2d68e8f3f2b8cd4
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-24 14:17:59 +0900

    Title: 스승의 은혜 3, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,23 @@
+n, b = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+mxm = 0
+for i in range(n):
+    tmp = lst[:]
+    tmp[i][0] = tmp[i][0] // 2
+    cost = []
+    for j in range(n):
+        cost.append(tmp[j][0] + tmp[j][1])
+    budget = b
+    j = 0
+    cnt = 0
+    while budget >= 0 and j < n:
+        budget -= cost[j]
+        j += 1
+        cnt += 1
+    if j == n and budget >= 0:
+        mxm = cnt
+        break
+    else:
+        mxm = max(mxm, cnt - 1)
+print(mxm)
