f8cffd6677ceb557f98c6714b65331eea2c1b2ad
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 10:02:03 +0900

    Title: A, B, C, D 찾기 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240725/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240725/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,5 +1,6 @@
 lst = list(map(int, input().split()))
 abcd = max(lst)
+lst.sort()
 
 n = len(lst)
 stat = True
@@ -10,5 +11,8 @@ for i in range(n):
                 if i == j or j == k or k == l or l == i or i == k or j == l or stat == False:
                     continue
                 if lst[i] + lst[j] + lst[k] + lst[l] == abcd and lst[i] <= lst[j] <= lst[k] <= lst[l]:
-                    print(lst[i], lst[j], lst[k], lst[l])
-                    stat =False
+                    tmp_lst = [lst[i], lst[j], lst[k], lst[l], lst[i]+lst[j], lst[i]+lst[k], lst[i]+lst[l],lst[j]+lst[k], lst[j]+lst[l],lst[k]+lst[l],lst[i]+lst[j]+lst[k],lst[i]+lst[j]+lst[l],lst[i]+lst[k]+lst[l],lst[l]+lst[j]+lst[k], abcd]
+                    tmp_lst.sort()
+                    if tmp_lst == lst:
+                        print(lst[i], lst[j], lst[k], lst[l])
+                        stat = False
