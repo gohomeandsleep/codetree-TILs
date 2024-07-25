ef35d287dfcd64bddcbafeb21ced87d0d008b339
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 16:05:56 +0900

    Title: 연속된 숫자 만들기 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240725/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,7 +3,7 @@ lst.sort()
 
 if lst[0] + 1 == lst[1] and lsr[1] + 1 == lst[2]:
     print(0)
-elif lst[0] + 1 == lst[1] or lst[1] + 1 == lst[2]:
+elif lst[1] - lst[0] < 2 or lst[2] - lst[1] < 2:
     print(1)
 else:
     print(2)
