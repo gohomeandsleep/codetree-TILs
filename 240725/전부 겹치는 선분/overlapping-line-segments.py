b8a5fa8bd7cf56cb25072a6e4474dbd3ce942e9f
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 13:49:53 +0900

    Title: 전부 겹치는 선분, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240725/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = [0 for _ in range(101)]
+for _ in range(n):
+    stp, endp = map(int, input().split())
+    for i in range(stp, endp+1):
+        lst[i] += 1
+
+res = 'No'
+for i in range(101):
+    if lst[i] == n:
+        res = 'Yes'
+print(res)
