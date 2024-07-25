b62b617e8cd7775025bd6ad870725690b9b87554
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 09:28:36 +0900

    Title: 훌륭한 점프, Time: 5093ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240725/훌륭한 점프/great-jump.py
+++ 240725/훌륭한 점프/great-jump.py
@@ -1,12 +1,18 @@
+import time
+
 n, k = map(int, input().split())
 lst = list(map(int, input().split()))
 
 m = lst[0]
 while len(lst) > 1:
     min_t = min(lst[1:k+1])
-    #print(lst[1:k+1])
-    min_k = lst.index(min_t)
+    #print(lst[1:k+1], lst)
+    if lst[0] == lst[1]:
+        min_k = 1
+    else:
+        min_k = lst.index(min_t)
     m = max(m, min_t)
     lst = lst[min_k:]
     #print(min_t)
+    #time.sleep(1)
 print(m)
