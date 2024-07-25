f820055a7dfcfd29aebedba66bb3a082da3dd34d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 16:43:29 +0900

    Title: 두 번째로 작은 수의 위치, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240725/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -3,12 +3,14 @@ lst = list(map(int, input().split()))
 
 tmp = lst[:]
 smallest = min(tmp)
-for i in tmp:
-    if i == smallest:
-        tmp.remove(i)
+for i in range(n):
+    #print(i)
+    if tmp[i] == smallest:
+        tmp[i] = 100
+#print(tmp)
 second = min(tmp)
 
-if lst.count(second) >= 2:
+if lst.count(second) >= 2 or lst.count(second) == 0:
     print(-1)
 else:
     print(lst.index(second) + 1)
