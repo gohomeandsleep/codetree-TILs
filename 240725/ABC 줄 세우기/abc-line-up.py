a8d8bca58d6e834c378e681bf726f1fa0d52fb9d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 16:30:51 +0900

    Title: ABC 줄 세우기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/ABC 줄 세우기/abc-line-up.py
+++ 240725/ABC 줄 세우기/abc-line-up.py
@@ -3,9 +3,9 @@ lst = list(input().split())
 
 res = 0
 for i in range(n):
-    for j in range(n-i):
+    for j in range(n-i-1):
         #print(lst[i], lst[j])
-        if ord(lst[i]) > ord(lst[j]):
-            lst[i], lst[j] = lst[j], lst[i]
+        if ord(lst[j]) > ord(lst[j+1]):
+            lst[j+1], lst[j] = lst[j], lst[j+1]
             res += 1
 print(res)
