30add6d3ffcb42af5e7e2877222f9830e3b89c29
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 15:10:42 +0900

    Title: 코딩톡, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/코딩톡/coding-talk.py
+++ 240725/코딩톡/coding-talk.py
@@ -1,13 +1,22 @@
 people, talk, check = map(int, input().split())
 lst = [chr(65+i) for i in range(people)]
+chat = [list(input()) for _ in range(talk)]
 
-for i in range(talk):
-    person, unseen = input().split()
-
-    if i+1 == check and int(unseen) == 0:
-        lst = []
-        break
-    if i >= check - 1:
-        if person in lst:
-            lst.remove(person)
-print(*lst, sep=' ')
+if chat[check-1][2] == '0': #안 읽은 사람이 0명인 경우 -> 출력할게 없음
+    print('')
+else:
+    lst.remove(chat[check-1][0])
+    for i in range(check, people):
+        try:
+            lst.remove(chat[i][0])
+        except:
+            print('', end='')
+    #print(lst)
+    for j in range(0, check-1):
+        if chat[j][2] == chat[check-1][2]:
+            #print(chat[j][2], chat[check-1][2])
+            try:
+                lst.remove(chat[j][0])
+            except:
+                print('', end='')
+    print(*lst, sep=' ')
