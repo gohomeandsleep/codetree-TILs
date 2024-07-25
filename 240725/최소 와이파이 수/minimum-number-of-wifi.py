40b09c67cc0d93043a541ab52a8855ccb8d2fffb
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 17:41:23 +0900

    Title: 최소 와이파이 수, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240725/최소 와이파이 수/minimum-number-of-wifi.py
@@ -3,10 +3,10 @@ lst = list(map(int, input().split()))
 
 res = 0
 i = m
-if m >= n // 2:
-    res = 1
-elif sum(lst) == 0:
+if sum(lst) == 0:
     res = 0
+elif m >= n // 2:
+    res = 1
 else:
     while i < n:
         #print(i)
