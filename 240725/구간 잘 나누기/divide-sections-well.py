26c2ca2fb451c7b4263cdef41a1e724da3e1de07
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-25 13:08:17 +0900

    Title: 구간 잘 나누기, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240725/구간 잘 나누기/divide-sections-well.py
+++ 240725/구간 잘 나누기/divide-sections-well.py
@@ -1,18 +1,19 @@
-import itertools
-
 n, k = map(int, input().split())
 lst = list(map(int, input().split()))
-lstk = [i+1 for i in range(n-1)]
-partition = itertools.combinations(lstk, k-1)
 
-res = float('inf')
-for p in partition:
-    tmp_p = list(p)
-    tmp = 0
-    maxp = 0
-    for i in range(k-1):
-        maxp = max(maxp, sum(lst[tmp:tmp_p[i]]))
-        tmp = tmp_p[i]
-    maxp = max(maxp, sum(lst[tmp:]))
-    res = min(res, maxp)
-print(res)
+max_sum = 10001
+
+for i in range(max(lst), max_sum):
+    part = 1  # 구간의 개수
+    p_sum = 0  # 부분합
+
+    for j in range(n):
+        if p_sum + lst[j] > i:
+            part += 1
+            p_sum = lst[j]
+        else:
+            p_sum += lst[j]
+
+    if part <= k:
+        print(i)
+        break
