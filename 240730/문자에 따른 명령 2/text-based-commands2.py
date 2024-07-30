6ee78c1293e27321be002e8a68649b94280e10b5
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-07-30 09:21:25 +0900

    Title: 문자에 따른 명령 2, Time: 124ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240730/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,17 @@
+inst = list(input())
+
+x,y, rot = 0, 0, 0
+
+rot_x = [0, 1, 0, -1]
+rot_y = [1, 0, -1, 0]
+
+for i in range(len(inst)):
+    if inst[i] == 'L':
+        rot -= 1
+    elif inst[i] == 'R':
+        rot += 1 
+    else:
+        x += rot_x[rot % 4]
+        y += rot_y[rot % 4]
+
+print(x, y)
