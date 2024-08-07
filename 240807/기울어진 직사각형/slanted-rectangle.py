2ddf948d84de1d6c7eb37101f02377eb2e6fb7d3
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-08-07 17:29:38 +0900

    Title: 기울어진 직사각형, Time: 377ms, Memory: 28MB, Status: Passed - CodeTree

--- 240807/기울어진 직사각형/slanted-rectangle.py
+++ 240807/기울어진 직사각형/slanted-rectangle.py
@@ -1,42 +1,26 @@
 n = int(input())
 lst = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1, 1, 1, -1]
-dy = [1, 1, -1, -1]
-
-def in_range(y, x):
-    return 0 <= x < n and 0 <= y < n and lst[y][x] != 0
-
 res = 0
-if n % 2 == 0:  # n이 짝수일 때는 첫 줄 탐색만으로 가장 큰 값을 알 수 있다.
-    for i in range(1, n-1):
-        direction = 0
-        p_sum = 0
-        tmpx, tmpy = i, 0
-        while direction < 4:
-            if in_range(tmpy + dy[direction], tmpx + dx[direction]):
-                tmpx += dx[direction]
-                tmpy += dy[direction]
-                p_sum += lst[tmpy][tmpx]
-            else:
-                direction += 1
-        res = max(res, p_sum)
-else:
-    """
-    n이 홀수일 때는 첫 줄 탐색만으로 가장 큰 값을 알 수 없다.
-    중심을 통과하는 수 중 가장 큰 값을 탐색해야 함
-    """
-    for i in range(1, n-1):
-        direction = 0
-        p_sum = 0
-        tmpx, tmpy = i, 0
-        while direction < 4:
-            if in_range(tmpy + dy[direction], tmpx + dx[direction]):
-                tmpx += dx[direction]
-                tmpy += dy[direction]
-                p_sum += lst[tmpy][tmpx]
-            else:
-                direction += 1
-        res = max(res, p_sum)
-    
+for i in range(1, n-1):  # y 시작 좌표
+    tmp_lst = [[0 for _ in range(i + 1)] for _ in range(n - i)]
+    tmpx = 0
+    tmpy = i
+    for j in range(n - i):  # tmp_lst y 좌표
+        for k in range(i + 1):  # tmp_lst x 좌표
+            if tmpy - k >= 0 and tmpx + k < n:
+                tmp_lst[j][k] = lst[tmpy - k][tmpx + k]
+        tmpx += 1
+        tmpy += 1
+    for stpy in range(n - i):
+        for endy in range(stpy, n - i):
+            for stpx in range(i + 1):
+                for endx in range(stpx, i + 1):
+                    tmp_v = 0
+                    for k in range(stpy, endy + 1):
+                        for l in range(stpx, endx + 1):
+                            if k == stpy or k == endy or l == stpx or l == endx:
+                                tmp_v += tmp_lst[k][l]
+                    res = max(res, tmp_v)
+
 print(res)
