8d638f139600ddb7708b86423e0639737d26dc0d
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-08-07 11:56:31 +0900

    Title: 행복한 수열의 개수, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 240807/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240807/행복한 수열의 개수/number-of-happy-sequence.py
@@ -6,16 +6,17 @@ for i in range(n):
     tmp_lst = lst[i]
     prev = tmp_lst[0]
     cnt = 1
+    stat = True
     for j in range(1, n):
         if tmp_lst[j] == prev:
             cnt += 1
         else:
-            if cnt >= k:
+            if cnt >= k and stat == True:
                 res += 1
-                break
+                stat = False
             prev = tmp_lst[j]
             cnt = 1
-    if cnt >= k:
+    if cnt >= k and stat == True:
         res += 1
 
 for i in range(n):
@@ -24,16 +25,17 @@ for i in range(n):
         tmp_lst.append(lst[j][i])
     prev = tmp_lst[0]
     cnt = 1
+    stat = True
     for j in range(1, n):
         if tmp_lst[j] == prev:
             cnt += 1
         else:
-            if cnt >= k:
+            if cnt >= k and stat == True:
                 res += 1
-                break
+                stat = False
             prev = tmp_lst[j]
             cnt = 1
-    if cnt >= k:
+    if cnt >= k and stat == True:
         res += 1
 
 print(res)
