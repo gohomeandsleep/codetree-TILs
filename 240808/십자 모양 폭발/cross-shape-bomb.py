5026dee992d0be61306dfacedc5e58cd84f6786c
gohomeandsleep <97226548+gohomeandsleep@users.noreply.github.com>
2024-08-08 13:39:24 +0900

    Title: 십자 모양 폭발, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,29 @@
+n = int(input())
+lst = [list(map(int, input().split())) for _ in range(n)]
+y, x = map(int, input().split())
+r = lst[y-1][x-1] - 1  # The blast radius excluding the bomb's own cell
+lst[y-1][x-1] = 0
+
+def in_range(y, x):
+    return 0 <= y < n and 0 <= x < n
+
+for i in range(-r, r + 1):
+    for j in range(-r, r + 1):
+        if in_range(y - 1 + i, x - 1 + j):
+            if abs(i) + abs(j) <= r:
+                if i == 0 or j == 0: 
+                    lst[y - 1 + i][x - 1 + j] = 0
+
+for i in range(n):
+    tmp_lst = []
+    for j in range(n-1, -1, -1):
+        tmp_lst.append(lst[j][i])
+    for j in range(n):
+        if tmp_lst[j] == 0:
+            tmp_lst.append(tmp_lst.pop(j))
+    #print(tmp_lst)
+    for j in range(n):
+        lst[n-j-1][i] = tmp_lst[j]
+
+for arr in lst:
+    print(*arr, sep=' ')
